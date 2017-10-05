398d654dc7e698d73a90b3469a7f05b0e817e8fd
Jason Juhyun Kim <mtinet@hanmail.net>
2017-10-06 01:35:40 +0900

    Create checkGrade.py

---
+++ checkGrade.py
@@ -0,0 +1,9 @@
+score = int(input('Input your grade : '))
+
+if score >= 90 : grade = 'A'
+elif score >= 80 : grade = 'B'
+elif score >= 70 : grade = 'C'
+else : grade = 'F'
+
+print('Your grade is %s' %(grade))
+           
