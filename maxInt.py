512c1d9cb9834555cf592f7c0fadc63efd487e30
Jason Juhyun Kim <mtinet@hanmail.net>
2017-10-07 03:22:11 +0900

    Create maxInt.py

---
+++ maxInt.py
@@ -0,0 +1,27 @@
+inputInt1 = int(input('첫 번째 정수를 입력하세요 : '))
+inputInt2= int(input('두 번째 정수를 입력하세요 : '))
+inputInt3 = int(input('세 번째 정수를 입력하세요 : '))
+inputInt4 = int(input('네 번째 정수를 입력하세요 : '))
+inputInt5 = int(input('다섯 번째 정수를 입력하세요 : '))
+
+maxInt = 0
+
+
+if maxInt < inputInt1 :
+    maxInt = inputInt1
+if maxInt < inputInt2 :
+    maxInt = inputInt2
+if maxInt < inputInt3 :
+    maxInt = inputInt3
+if maxInt < inputInt4 :
+    maxInt = inputInt4
+if maxInt < inputInt5 :
+    maxInt = inputInt5
+
+print('입력된 정수들 중 가장 큰 값은 %d입니다.'%(maxInt))
+
+    
+    
+            
+    
+    
