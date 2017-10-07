97efff6b02fef0d0478db9d7597a10e5287f5781
Jason Juhyun Kim <mtinet@hanmail.net>
2017-10-07 12:57:41 +0900

    Create expressMainFunction.py

---
+++ expressMainFunction.py
@@ -0,0 +1,11 @@
+def findMax(a, b) :
+    if a>b : return a
+    else : return b
+
+def main() :
+    x = findMax(1, 4)
+    y = findMax(10, 30)
+    print(x, y)
+
+if __name__ == '__main__' :
+    main()
