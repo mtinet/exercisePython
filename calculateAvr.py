d3ef104f57771beec2014e9428cb971b13b63a67
Jason Juhyun Kim <mtinet@hanmail.net>
2017-10-07 11:50:05 +0900

    Update calculateAvr.py

--- calculateAvr.py
+++ calculateAvr.py
@@ -1,3 +1,6 @@
+print('각 항목별 국어, 수학, 영어의 성적은 다음 dict와 같음')
+print()
+
 score = {1:[80, 90,86],
          2:[78,88,85],
          3:[85,85,92],
@@ -5,6 +8,8 @@ score = {1:[80, 90,86],
          5:[90,95,100]}
 
 print(score)
+print()
+
 for key, value in score.items() :
     print('%d번 -'%(key), '평균 : %.2f'%(sum(value) /len(value)))
     
