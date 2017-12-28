9a2b5d7e8ef1c9adcdcbea5b2f6366e336e215b8
Jason Juhyun Kim <mtinet@hanmail.net>
2017-12-28 12:13:08 +0900

    Create responsiveGUI.py

---
+++ responsiveGUI.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack(fill=BOTH, expand=1)
+
+        listbox = Listbox(frame)
+        for item in ['red', 'green', 'blue', 'yellow', 'pink']:
+            listbox.insert(END, item)
+        listbox.grid(row=0, column=0, sticky=W+E+N+S)
+
+        text = Text(frame, relief=SUNKEN)
+        text.grid(row=0, column=1, sticky=W+E+N+S)
+        text.insert(END, 'word ' * 100)
+        frame.columnconfigure(1, weight=1)
+        frame.rowconfigure(0, weight=1)
+
+root = Tk()
+app = App(root)
+root.geometry("400*300+0+0")
+root.mainloop()
