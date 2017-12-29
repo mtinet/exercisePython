b5bf531099ba4fa509d11b5e4514379ede135439
Jason Juhyun Kim <mtinet@hanmail.net>
2017-12-29 15:59:22 +0900

    Create buttonAlert.py

---
+++ GUI/buttonAlert.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import tkinter.messagebox as mb
+
+class App:
+    def __init__(self, master):
+        b = Button(master, text = 'Press Me', command = self.info).pack()
+
+    def info(self):
+        mb.showinfo('Infomation', "Please don't press that button again!!")
+
+root = Tk()
+app = App(root)
+root.mainloop()
